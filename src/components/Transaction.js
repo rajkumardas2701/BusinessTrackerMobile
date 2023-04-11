1e62b828983be2f75e51135210b80169c33bcaed
Rajkumar Das <rajkumardas2701@gmail.com>
2023-04-11 15:25:37 +0530

    add other transactions tab

---
+++ src/components/Transaction.js
@@ -0,0 +1,140 @@
+import React, {useContext} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import PropTypes from 'prop-types';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faEdit, faTrash} from '@fortawesome/free-solid-svg-icons';
+import DashboardContext from '../contexts/DashboardContext';
+
+const Transaction = ({
+  sTx,
+  setShowDeleteWarning,
+  showDeleteWarning,
+  setDeleteTxID,
+}) => {
+  const {setShowEditTransactionForm, showEditTransactionForm, setEditFormData} =
+    useContext(DashboardContext);
+
+  const handleEditForm = () => {
+    setEditFormData({
+      id: sTx.id,
+      date: sTx.date,
+      amount: sTx.amount,
+      send_receive: sTx.send_receive,
+      action_by: sTx.action_by,
+      remark: sTx.remark,
+      deal_id: sTx.deal_id,
+    });
+    setShowEditTransactionForm(!showEditTransactionForm);
+  };
+
+  const handleDeleteForm = () => {
+    setDeleteTxID(sTx.id);
+    setShowDeleteWarning(!showDeleteWarning);
+  };
+
+  return (
+    <View style={styles.containerSmall}>
+      <View style={styles.row1}>
+        <Text style={styles.dateSmall}>{sTx.date}</Text>
+        <Text style={styles.amountSmall}>{sTx.amount}</Text>
+        <Text
+          style={[
+            styles.sendReceiveSmall,
+            {color: sTx.send_receive === 'Sent' ? 'red' : 'green'},
+          ]}>
+          {sTx.send_receive}
+        </Text>
+        <TouchableOpacity
+          style={styles.editButtonSmall}
+          onPress={handleEditForm}>
+          <FontAwesomeIcon icon={faEdit} style={styles.editIconSmall} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.deleteButtonSmall}
+          onPress={handleDeleteForm}>
+          <FontAwesomeIcon icon={faTrash} style={styles.deleteIconSmall} />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.row2}>
+        <View style={styles.row2Item}>
+          <Text style={styles.boldText}>Action by:</Text>
+          <Text>{sTx.action_by}</Text>
+        </View>
+        <View style={styles.row2Item}>
+          <Text style={styles.boldText}>Remark:</Text>
+          <Text>{sTx.remark}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+Transaction.propTypes = {
+  sTx: PropTypes.oneOfType([PropTypes.object]).isRequired,
+  setShowDeleteWarning: PropTypes.func.isRequired,
+  showDeleteWarning: PropTypes.bool.isRequired,
+  setDeleteTxID: PropTypes.func.isRequired,
+};
+
+const styles = StyleSheet.create({
+  containerSmall: {
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: 'blue',
+    marginLeft: 10,
+  },
+  row1: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  dateSmall: {
+    flex: 0.6,
+    fontWeight: 800,
+  },
+  amountSmall: {
+    flex: 0.5,
+    fontWeight: 800,
+  },
+  sendReceiveSmall: {
+    flex: 1,
+    textAlign: 'center',
+    // paddingLeft: 30,
+    // marginRight: 10,
+  },
+  editButtonSmall: {
+    flex: 0.3,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 30,
+    height: 30,
+    // marginRight: 10,
+  },
+  editIconSmall: {
+    color: 'blue',
+  },
+  deleteButtonSmall: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 30,
+    height: 30,
+  },
+  deleteIconSmall: {
+    color: 'red',
+  },
+  row2: {
+    flexDirection: 'column',
+  },
+  row2Item: {
+    flex: 1,
+    flexDirection: 'row',
+    // marginRight: 10,
+  },
+  boldText: {
+    fontWeight: 'bold',
+    marginRight: 10,
+  },
+});
+
+export default Transaction;
--- src/containers/SideTransactions.js
+++ src/containers/SideTransactions.js
@@ -1,5 +1,128 @@
-import React, {Text} from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {View, Text, StyleSheet, ScrollView, Dimensions} from 'react-native';
+import Transaction from '../components/Transaction';
+import DashboardContext from '../contexts/DashboardContext';
+import DeleteWarning from '../layouts/DeleteWarning';
+import {filterSideTxs} from '../helper_functions/helperMethods';
 
-const SideTransactions = () => <Text>Side Transactions</Text>;
+const SideTransactions = () => {
+  const {txs} = useContext(DashboardContext);
+  const [sTxs, setSTxs] = useState([]);
+  const [showDeleteWarning, setShowDeleteWarning] = useState(false);
+  const [deleteTxID, setDeleteTxID] = useState(0);
+  useEffect(() => {
+    setSTxs(filterSideTxs(txs));
+  }, [txs]);
+
+  return (
+    <View style={styles.sideTransactionsContainer}>
+      <ScrollView style={styles.sideTransactionsList}>
+        <View style={styles.sideTransactionsTableHead}>
+          <View style={styles.sideTransactionsTableHeadContent}>
+            <Text style={styles.transactionFirstFour}>Date</Text>
+            <Text style={styles.transactionFirstFour}>Amount</Text>
+            <Text style={styles.transactionFirstFourSentReceive}>
+              Sent/Received
+            </Text>
+            <Text style={styles.editDelete}>Edit</Text>
+            <Text style={styles.editDelete}>Delete</Text>
+          </View>
+        </View>
+        <View style={styles.sideTransactionsTableBody}>
+          {sTxs && sTxs.length ? (
+            sTxs.map(sTx => (
+              <Transaction
+                key={sTx.id}
+                sTx={sTx}
+                setShowDeleteWarning={setShowDeleteWarning}
+                showDeleteWarning={showDeleteWarning}
+                setDeleteTxID={setDeleteTxID}
+              />
+            ))
+          ) : (
+            <View>
+              <Text style={styles.noTransactionsText}>
+                There's no other transactions to show now
+              </Text>
+            </View>
+          )}
+        </View>
+        {showDeleteWarning && (
+          <DeleteWarning fn={setShowDeleteWarning} deleteTxID={deleteTxID} />
+        )}
+      </ScrollView>
+    </View>
+  );
+};
+
+const {height} = Dimensions.get('window');
+const vh = height / 100;
+
+const styles = StyleSheet.create({
+  sideTransactionsContainer: {
+    width: 430,
+    marginLeft: 135,
+    paddingRight: 5,
+  },
+  sideTransactionsList: {
+    height: 75 * vh,
+  },
+  sideTransactionsTableHead: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    paddingBottom: 5,
+    marginBottom: 5,
+    // marginLeft: 10,
+  },
+  sideTransactionsTableHeadContent: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginLeft: 10,
+  },
+  transactionFirstFour: {
+    flex: 1.2,
+    fontWeight: 900,
+    width: 5,
+    textAlign: 'center',
+  },
+  transactionFirstFourSentReceive: {
+    // Add styles for transaction-first-four-sent-receive
+    flex: 2,
+    fontWeight: 900,
+    textAlign: 'center',
+  },
+  transactionFirstFourSmall: {
+    // Add styles for transaction-first-four-small
+    flex: 1,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+  transactionsRemarkSmall: {
+    // Add styles for transactions-remark-small
+    flex: 1,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+  editDelete: {
+    // Add styles for edit-delete
+    flex: 0.6,
+    textAlign: 'center',
+    fontWeight: 900,
+    fontSize: 14,
+  },
+  sideTransactionsTableBody: {
+    // Add styles for side-transactions-table-body
+  },
+  noTransactionsText: {
+    // Add styles for noTransactionsText
+    textAlign: 'center',
+    fontStyle: 'italic',
+    color: 'gray',
+  },
+});
 
 export default SideTransactions;
